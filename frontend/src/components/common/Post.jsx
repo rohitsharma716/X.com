6a3b62d6831e90f4e25bf582c9c68f745a9c451e
ROHIT SHARMA <102727841+rohitsharma716@users.noreply.github.com>
2025-05-02 23:35:23 +0530

    added delete post

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -3,21 +3,64 @@ import { BiRepost } from "react-icons/bi";
 import { FaRegHeart } from "react-icons/fa";
 import { FaRegBookmark } from "react-icons/fa6";
 import { FaTrash } from "react-icons/fa";
-import { useState } from "react";
+import { useState } from "react"; 
 import { Link } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import LoadingSpinner from "../common/LoadingSpinner";
+import { useQueryClient } from "@tanstack/react-query";
+
 
 const Post = ({ post }) => {
 	const [comment, setComment] = useState("");
+	// const {data : authUser} = useQuery({queryKey: ["authUser"]});
+	const { data: authUser } = useQuery({
+		queryKey: ["authUser"],
+		queryFn: async () => {
+		  const res = await fetch("/api/auth/me");
+		  const data = await res.json();
+		  if (!res.ok) throw new Error(data.error || "Failed to fetch user");
+		  return data;
+		},
+	  });
+	  
+	const queryClient = useQueryClient();
+
+	const { mutate:deletePost, isPending } = useMutation({
+		 mutationFn: async () => {
+			 try{
+				const  res =  await fetch(`/api/posts/${post._id}` , {
+					method: "DELETE",
+				})
+				const data =  await res.json();
+				if(!res.ok){
+					throw new Error(data.error || "Failed to delete post");
+				}
+				return data;
+
+			 }catch(error){
+				   throw new Error(error);	
+			 }
+		 },onSuccess: () => {
+			 toast.success("Post deleted successfully");
+			 queryClient.invalidateQueries({ queryKey: ["posts"] });
+			 //invalidate queries if comment is deleted
+		 }
+		 
+	});
 	const postOwner = post.user;
 	const isLiked = false;
 
-	const isMyPost = true;
+	const isMyPost = authUser?.username === postOwner.username;
 
 	const formattedDate = "1h";
 
 	const isCommenting = false;
 
-	const handleDeletePost = () => {};
+	const handleDeletePost = () => {
+		deletePost();
+	};
 
 	const handlePostComment = (e) => {
 		e.preventDefault();
@@ -45,7 +88,9 @@ const Post = ({ post }) => {
 						</span>
 						{isMyPost && (
 							<span className='flex justify-end flex-1'>
-								<FaTrash className='cursor-pointer hover:text-red-500' onClick={handleDeletePost} />
+								{!isPending && <FaTrash className='cursor-pointer hover:text-red-500' onClick={handleDeletePost} />}
+							     {isPending && (
+									<LoadingSpinner  size='sm'/>)}	
 							</span>
 						)}
 					</div>
